9cb84ad44a0f3c12972d9f119eac278123889858
Marcelino <marcelino_gamino@yahoo.com>
2022-05-13 00:41:41 -0700

    fourth commit

--- git_practice.js
+++ git_practice.js
@@ -1,3 +1,4 @@
 console.log("my first commit with Git!")
 console.log("so far so good!")
 console.log("lets see if we can backtrack.")
+console.log("messed around a bit with 'checkout', wasn't able to 'push' without getting errors... 'head detached'?")
